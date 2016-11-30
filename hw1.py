d9f3dd48a71b98a2d36d190679400b3a123a6f24
Aelia Kavington <aelia.kavington@gmail.com>
2016-11-30 23:09:16 +0300

    Homework 1

---
+++ hw1.py
@@ -0,0 +1,23 @@
+import pytest
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome(executable_path='./chromedriver')
+    print(wd.capabilities)
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_example(driver):
+    driver.get("http://www.google.com/")
+    driver.find_element_by_name("q").send_keys("webdriver")
+    driver.find_element_by_name("btnG").click()
+    WebDriverWait(driver, 10).until(EC.title_is("webdriver - Google Search"))
+    sleep(5)
+
+
