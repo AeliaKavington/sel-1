1d1af349a567a50c14a7e3ece2fa44e119ddb777
Aelia Kavington <aelia.kavington@gmail.com>
2016-12-05 00:02:37 +0300

    Homework 6

---
+++ hw6.py
@@ -0,0 +1,19 @@
+import pytest
+from time import sleep
+from selenium import webdriver
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Firefox(executable_path='./geckodriver',
+                           firefox_binary="/Applications/FirefoxNightly.app/Contents/MacOS/firefox-bin")
+    print(wd.capabilities)
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_example(driver):
+    driver.get("http://localhost/litecart/en/")
+    driver.find_element_by_name("email").send_keys("user@email.com")
+    driver.find_element_by_name("password").send_keys("test")
+    driver.find_element_by_name("login").click()
+    sleep(5)
